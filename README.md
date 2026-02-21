5890b22bb9de1377817c8aa2cc2cb3a5ad49f092
lambda liebert <longqt321@gmail.com>
2026-02-21 08:57:42 +0700

    gugugaga

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # machine_learning
+# This repo aim to entertain the owner. Nothing else
+
+I use 'uv' as my pip package manager.
+I use Fedora btw
---
+++ linear_regression.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+diabetes = datasets.load_diabetes()
+
+diabetes_X = diabetes.data[:,np.newaxis,2]
+
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+regr = linear_model.LinearRegression()
+
+regr.fit(diabetes_X_train,diabetes_y_train)
+
+dia_pred = regr.predict(diabetes_X_test)
+
+print(f"Coefficients: {regr.coef_}")
+print(f"MSE: {mean_squared_error(diabetes_y_test,dia_pred)}")
+
+print(f"Variance score: {r2_score(diabetes_y_test,dia_pred):.2f}")
+sns.regplot(x=diabetes_X_test.ravel(),
+            y=diabetes_y_test)
+plt.xlabel("BMI (scaled)")
+plt.ylabel("Disease progression")
+plt.title("Linear Regression on Diabetes dataset")
+plt.show()
